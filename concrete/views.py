0360f0da5137595b700e3e7999041f53a7aa2032
Jeobaz <kirill.manov@list.ru>
2020-02-11 18:07:57 +0300

    Server pull

--- concrete/views.py
+++ concrete/views.py
@@ -9,24 +9,29 @@ def index(request):
     #     print(img.url)
     return render(request, 'index.html', context={'images': images})
 
-@admin_required(login_url="/")
+# @admin_required(login_url="/")
 def createGallery(request):
-    if request.method == 'GET':
+    if request.method == 'GET' and request.user.is_superuser:
         return render(request, 'createGallery.html')
-    else:
+    elif request.user.is_superuser:
         for el in request.FILES.getlist('files'):
             obj = AlbumImage(image = el)
             obj.save()
         return render(request, 'createGallery.html')
+    else:
+        return redirect('index')
         
         
 def send(request):
     from django.core.mail import send_mail
-    send_mail(
-        "Новая заявка с качайбетон.рф",
-        f"Завяка от: {request.POST['email'] }, \nИмя: {request.POST['name'] }, \nТелефон: {request.POST['tel'] }  \n" + "Текст заявки: " + request.POST['message'],
-        'sender@xn--80aadqsd0ah9a8b.xn--p1ai',
-        ['709090@inbox.ru'],
-        fail_silently=False,
-    )
-    return redirect('index')
+    if request.method == 'POST':
+        send_mail(
+            "Новая заявка с качайбетон.рф",
+            f"Завяка от: {request.POST['email'] }, \nИмя: {request.POST['name'] }, \nТелефон: {request.POST['tel'] }  \n" + "Текст заявки: " + request.POST['message'],
+            'sender@xn--80aadqsd0ah9a8b.xn--p1ai',
+            ['709090@inbox.ru'],
+            fail_silently=False,
+        )
+        return redirect('index')
+    else: 
+        return redirect('index')
