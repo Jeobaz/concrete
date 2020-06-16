d006f0ff4500851271eab4acfd20f452ac60c9da
Jeobaz <kirill.manov@list.ru>
2020-06-16 07:37:46 +0300

    Images to webpw

--- concrete/forms.py
+++ concrete/forms.py
@@ -15,25 +15,27 @@ class modalTicket(forms.Form):
     name = forms.CharField(min_length=3,
                                max_length=200,
                                required=True,
-                               help_text='Длина логина должна быть не меньше 3-х и не больше 20',
+                               help_text='Ваше имя',
                                widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'form34', 'name':'name'}),
                                )
     email = forms.EmailField(max_length=255,
                                 required=True,
                                 widget=forms.EmailInput(attrs={'class': 'form-control validate', 'id': 'form29', 'name':'email'}),
                                 help_text='Это поле обязательно')
-    tel = forms.CharField(min_length=6,
-                                max_length=11,
-                                widget=forms.TextInput(attrs={'class': 'form-control validate', 'id': 'form8', 'name':'tel', 'type': 'tel'}),
+    tel = forms.CharField(required=False,
+                                min_length=6,
+                                max_length=12,
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'phone', 'name':'tel', 'type': 'tel'}),
                                )
     message = forms.CharField(min_length=3,
-                                max_length=2000,
-                                help_text='Длина логина должна быть не меньше 3-х и не больше 20',
+                                required=False,
+                                max_length=1000,
+                                help_text='Ваше сообщение',
                                 widget=forms.Textarea(attrs={'class': 'md-textarea form-control', 'id': 'form7', 'name':'message', 'rows': '3'}),
                                 )
 
     class Meta:
-        fields = ('name', 'email', 'phone', 'message', 'captcha')
+        fields = ('name', 'email', 'tel', 'message', 'captcha')
 
     
 
